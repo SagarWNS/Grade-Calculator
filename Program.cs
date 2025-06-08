9f06d6c3abe4abc4aaa16212f93af697b7ab0a36
Sagar R <u499161@wns.com>
2025-06-08 22:56:40 +0530

    Second Version

--- Program.cs
+++ Program.cs
@@ -1,56 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-const int CreditH_ENG = 3, CreditH_Alg = 3, CreditH_Bio = 4, CreditH_CS = 4, CreditH_Psy = 3;
-
-
-string Student_Names()
+﻿public class Students
 {
-    Console.WriteLine("Enter the Student Name: ");
-    string Name = Console.ReadLine();
-    return Name;
+    public string name { set; get; }
+    public Courses[] course_details = new Courses[5];
 }
 
-double Student_grades()
+public class Courses
 {
-    Console.WriteLine("Enter your English Grade:");
-    int English_Grade = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter your Algebra Grade:");
-    int Alg_Grade = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter your Biology Grade:");
-    int Bio_Grade = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter your CS Grade:");
-    int CS_Grade = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter your Psychology Grade:");
-    int Psy_Grade = Convert.ToInt32(Console.ReadLine());
-
-    int Credit_sum = CreditH_ENG + CreditH_Alg + CreditH_Bio + CreditH_CS + CreditH_Psy;
-    int Sum = (English_Grade * CreditH_ENG) + (Alg_Grade * CreditH_Alg) + (Bio_Grade * CreditH_Bio) + (CS_Grade * CreditH_CS) + (Psy_Grade * CreditH_Psy);
-
-    double GPA = Math.Round((double)Sum / (double)Credit_sum, 2);
-    Console.WriteLine("*****************");
-    return GPA;
+    public string name { set; get; }
+    public double grade { set; get; }
+    public int credits { set; get; }
 }
 
-Console.WriteLine("How many Students GPA you want to calculate? :");
-int num = Convert.ToInt32(Console.ReadLine());
-
-string[] Names = new string[num];
-double[] GPAs = new double[num];
-
-for (int i = 0; i < num; i++)
+public class Program
 {
-    Names[i] = Student_Names();
-    GPAs[i] = Student_grades();
+    public static void Main()
+    {
+        Console.WriteLine("Enter the Name of the student: ");
+        string student_name = Console.ReadLine();
+
+        Students Student1 = new Students { name = student_name };
+        double[] Grade_product = new double[5];
+        int Credit_sum = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Enter the name of the course: ");
+            string course_name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Grade you got: ");
+            double course_grade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the credits of the course: ");
+            int course_credit = Convert.ToInt32(Console.ReadLine());
+
+            Courses course = new Courses { name = course_name, grade = course_grade, credits = course_credit };
+            Student1.course_details[i] = course;
+            Grade_product[i] = course.grade * course.credits;
+            Credit_sum = Credit_sum + course.credits;
+        }
+
+        double sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum = sum + Grade_product[i];
+        }
+
+        double Final_GPA = Math.Round(sum / Credit_sum,2);
+        Console.WriteLine(sum);
+        Console.WriteLine(Credit_sum);
+
+        Console.WriteLine("Student Name: " + Student1.name);
+        Console.WriteLine("""Courses            Grades            Credits""");
+        for (int i = 0; i < 5; i++)
+        {
+            // Using String formatting I've Printed the Table.
+            // {Var, -24} here The "24" signifies that the Variable field should be 24 spaces wide.
+            // The " - " symbol is to left align it in that user-defined 20 space width.
+
+            Console.WriteLine($"{Student1.course_details[i].name, -24}{Student1.course_details[i].grade, -16} {Student1.course_details[i].credits}");
+        }
+        Console.WriteLine("Final GPA: " + Final_GPA);
+    }
 }
-
-for (int i = 0; i < num; i++)
-{
-    Console.WriteLine((i+1) +". "+ Names[i] + " : " + GPAs[i]);
-}
-Console.WriteLine("*****************");
-
-
-
